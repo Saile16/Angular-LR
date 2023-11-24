6fac6d2f79079752203ce5d8c3841ade33c2a2f3
unknown <alrlucano@gmail.com>
2023-11-23 23:42:48 -0500

    list-component y search box uso de viewChild revisar

--- 03-gifs-app/src/app/gifs/components/search-box/search-box.component.ts
+++ 03-gifs-app/src/app/gifs/components/search-box/search-box.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'gifs-search-box',
   template: ` <h5>Buscar:</h5>
-    <input type="text" class="form-control" placeholder="Buscar gifs" />`,
+    <input
+      type="text"
+      class="form-control"
+      placeholder="Buscar gifs"
+      (keyup.enter)="searchTag()"
+      #txtTagInput
+    />`,
 })
-export class SearchBoxComponent {}
+export class SearchBoxComponent {
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+  constructor() {}
+
+  // searchTag(newTag: string) {
+  searchTag() {
+    const newTag = this.tagInput.nativeElement.value;
+    console.log(newTag);
+  }
+}
