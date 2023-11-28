3aabee430a89bc5ff1c5c1f8ca17bb9474544451
unknown <alrlucano@gmail.com>
2023-11-28 00:45:46 -0500

    Agregamos localStorage

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -8,7 +8,9 @@ export class GifsService {
   private _tagsHistory: string[] = [];
   private apiKey: string = 'YyN1O33w6eu5soIW6k2aHEaFBnsG3EoH';
   private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.loadLocalStorage();
+  }
 
   get tagsHistory() {
     return [...this._tagsHistory];
@@ -23,6 +25,18 @@ export class GifsService {
     //insertamos el tag actualizado ya que ya no existe el tag dentro de la lista
     this._tagsHistory.unshift(tag);
     this._tagsHistory = this.tagsHistory.splice(0, 10);
+    this.saveLocalStorage();
+  }
+
+  private saveLocalStorage(): void {
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+  }
+
+  private loadLocalStorage(): void {
+    if (!localStorage.getItem('history')) return;
+    this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+    if (this._tagsHistory.length === 0) return;
+    this.searchTag(this._tagsHistory[0]);
   }
 
   searchTag(tag: string): void {
