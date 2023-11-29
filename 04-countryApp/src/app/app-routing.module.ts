c9e7266ac5d28bef3a41c08da6e75d8d5c624851
unknown <alrlucano@gmail.com>
2023-11-29 00:12:30 -0500

    Creamos otra pag , recordar importaciones de router para que funcione y comandos para el router , routerLink y demas

--- 04-countryApp/src/app/app-routing.module.ts
+++ 04-countryApp/src/app/app-routing.module.ts
@@ -2,19 +2,25 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './shared/pages/home-page/home-page.component';
 import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
+import { ContactPageComponent } from './shared/pages/contact-page/contact-page.component';
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: '',
     component: HomePageComponent,
+    children: [],
   },
   {
     path: 'about',
     component: AboutPageComponent,
   },
   {
+    path: 'contact',
+    component: ContactPageComponent,
+  },
+  {
     path: '**',
-    redirectTo: 'home',
+    redirectTo: '',
   },
 ];
 
