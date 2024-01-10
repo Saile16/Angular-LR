0e7b24d48a4742ddaecda1f9f07eb7111f9eb2ff
Alexander Lucano <alrlucano@gmail.com>
2024-01-10 00:07:28 -0500

    Agregamos un routing , por si no encuentra el pais, tambien usamos switchMap revisar todo eso

--- 04-countryApp/src/app/countries/services/countries.service.ts
+++ 04-countryApp/src/app/countries/services/countries.service.ts
@@ -8,6 +8,14 @@ export class CountriesService {
   private apiUrl: string = 'https://restcountries.com/v3.1/';
   constructor(private http: HttpClient) {}
 
+  searchCountryByAlphaCode(code: string): Observable<Country | null> {
+    const url = `${this.apiUrl}/alpha/${code}`;
+    return this.http.get<Country[]>(url).pipe(
+      map((countries) => (countries.length > 0 ? countries[0] : null)),
+      catchError((error) => of(null))
+    );
+  }
+
   searchCapital(term: string): Observable<Country[]> {
     const url = `${this.apiUrl}/capital/${term}`;
     return this.http.get<Country[]>(url).pipe(catchError((error) => of([])));
